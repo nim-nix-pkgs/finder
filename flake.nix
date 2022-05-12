de8209267f00ac6978550ba1c8e7d6048e7b03ef
hugosenari <hugosenari@gmail.com>
2022-05-12 11:51:35 -0300

    chore: re index finder

--- flake.nix
+++ flake.nix
@@ -7,6 +7,30 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."finder-master".dir   = "master";
+  inputs."finder-master".owner = "nim-nix-pkgs";
+  inputs."finder-master".ref   = "master";
+  inputs."finder-master".repo  = "finder";
+  inputs."finder-master".type  = "github";
+  inputs."finder-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."finder-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."finder-v0_1_1".dir   = "v0_1_1";
+  inputs."finder-v0_1_1".owner = "nim-nix-pkgs";
+  inputs."finder-v0_1_1".ref   = "master";
+  inputs."finder-v0_1_1".repo  = "finder";
+  inputs."finder-v0_1_1".type  = "github";
+  inputs."finder-v0_1_1".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."finder-v0_1_1".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."finder-v0_2_0".dir   = "v0_2_0";
+  inputs."finder-v0_2_0".owner = "nim-nix-pkgs";
+  inputs."finder-v0_2_0".ref   = "master";
+  inputs."finder-v0_2_0".repo  = "finder";
+  inputs."finder-v0_2_0".type  = "github";
+  inputs."finder-v0_2_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."finder-v0_2_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
